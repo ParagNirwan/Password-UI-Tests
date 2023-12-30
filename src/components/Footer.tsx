10ad0934e2ec666ca702442064c65dab0851a08b
ParagNirwan <52133765+ParagNirwan@users.noreply.github.com>
2023-12-30 03:49:38 +0100

    Removed Marquee

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,7 @@ function Footer() {
       className="bg-dark-subtle text-dark text-center container fixed-bottom"
       style={{ borderRadius: "4px" }}
     >
-      <span>
-        I'll remove the animation later, please let me have some fun you mofo
-      </span>
-      <marquee>For Interactive Systems | Built by Madhura and Parag</marquee>
+      For Interactive Systems | Built by Madhura and Parag
     </div>
   );
 }
